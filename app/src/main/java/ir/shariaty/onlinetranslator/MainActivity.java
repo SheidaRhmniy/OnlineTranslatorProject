ef8babba3b314727c6ca5ef2e391d87a3fe205c2
Sheida <Sheida.Rahmaniy@gmail.com>
2021-12-21 19:44:48 +0330

    splash screen commit

--- app/src/main/java/ir/shariaty/onlinetranslator/MainActivity.java
+++ app/src/main/java/ir/shariaty/onlinetranslator/MainActivity.java
@@ -2,13 +2,27 @@ package ir.shariaty.onlinetranslator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+
+    Handler h=new Handler();
+    private final int Splash_Display_Lentgh=3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(MainActivity.this,HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
     }
 }
